9a2c66944ef887c5b36084aa4f1fb9917c14124c
Sunriver <seriousman1994217winter@gmail.com>
2023-04-03 13:24:09 +0400

    initial project

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const GUN = require('gun');
+
+const route = require("./app/routes/routes.js");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors("*"));
+
+app.use(express.json()); 
+
+app.use(express.urlencoded({ extended: true })); 
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+route(app);
+
+// var gun = GUN({
+//   file: 'data.json'
+// });
+// gun.wsp(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+
+
+const gun = GUN({web: server});
+
+gun.get('mark').put({boss: 'Hello'});
+
+gun.get('mark').get('boss').get('name').once(function(data, key){
+  // `once` grabs the data once, no subscriptions.
+  console.log("Mark's boss is", data);
+});
